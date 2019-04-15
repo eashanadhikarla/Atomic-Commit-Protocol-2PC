6ce9c1853a4c5239eca9264b2ba7c5c9aac8183b
Eashan Adhikarla <aeashan@gmail.com>
2019-04-14 21:26:01 -0400

    Delete run_all.bat

--- run_all.bat
+++
@@ -1,7 +0,0 @@
-for host in ip-172-31-24-210 ip-172-31-22-126 ip-172-31-1-179 ip-172-31-12-241 ip-172-31-13-207 ; do 
-   ssh $host ./RHT/threadpool_server
-done
-
-for host in ip-172-31-24-210 ip-172-31-22-126 ip-172-31-1-179 ip-172-31-12-241 ip-172-31-13-207 ; do 
-   ssh $host ./RHT/client_cordinator
-done
